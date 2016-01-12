f4b695f1135dd76cc55718faa96f82d3f7b13b13
gcanti <giulio.canti@gmail.com>
2016-01-12 19:50:24 +0100

    pass the whole options object to validators

--- index.js
+++ index.js
@@ -61,81 +61,81 @@ ValidationResult.prototype.toString = function () {
 function validate(x, type, options) {
   options = options || {};
   var path = t.Array.is(options) ? options : options.path || [];
-  return new ValidationResult(recurse(x, type, path, options.context));
+  return new ValidationResult(recurse(x, type, path, options));
 }
 
-function recurse(x, type, path, context) {
+function recurse(x, type, path, options) {
   if (t.isType(type)) {
-    return validators[type.meta.kind](x, type, path, context);
+    return validators[type.meta.kind](x, type, path, options);
   }
   else {
-    return validators.es6classes(x, type, path, context);
+    return validators.es6classes(x, type, path, options);
   }
 }
 
 var validators = validate.validators = {};
 
-validators.es6classes = function validateES6Classes(x, type, path, context) {
+validators.es6classes = function validateES6Classes(x, type, path, options) {
   return {
     value: x,
-    errors: x instanceof type ? [] : [ValidationError.of(x, type, path, context)]
+    errors: x instanceof type ? [] : [ValidationError.of(x, type, path, options.context)]
   };
 };
 
 // irreducibles and enums
 validators.irreducible =
-validators.enums = function validateIrreducible(x, type, path, context) {
+validators.enums = function validateIrreducible(x, type, path, options) {
   return {
     value: x,
-    errors: type.is(x) ? [] : [ValidationError.of(x, type, path, context)]
+    errors: type.is(x) ? [] : [ValidationError.of(x, type, path, options.context)]
   };
 };
 
-validators.list = function validateList(x, type, path, context) {
+validators.list = function validateList(x, type, path, options) {
 
   // x should be an array
   if (!t.Arr.is(x)) {
-    return {value: x, errors: [ValidationError.of(x, type, path, context)]};
+    return {value: x, errors: [ValidationError.of(x, type, path, options.context)]};
   }
 
   var ret = {value: [], errors: []};
   // every item should be of type `type.meta.type`
   for (var i = 0, len = x.length; i < len; i++ ) {
-    var item = recurse(x[i], type.meta.type, path.concat(i), context);
+    var item = recurse(x[i], type.meta.type, path.concat(i), options);
     ret.value[i] = item.value;
     ret.errors = ret.errors.concat(item.errors);
   }
   return ret;
 };
 
-validators.subtype = function validateSubtype(x, type, path, context) {
+validators.subtype = function validateSubtype(x, type, path, options) {
 
   // x should be a valid inner type
-  var ret = recurse(x, type.meta.type, path, context);
+  var ret = recurse(x, type.meta.type, path, options);
   if (ret.errors.length) {
     return ret;
   }
 
   // x should satisfy the predicate
   if (!type.meta.predicate(ret.value)) {
-    ret.errors = [ValidationError.of(x, type, path, context)];
+    ret.errors = [ValidationError.of(x, type, path, options.context)];
   }
 
   return ret;
 
 };
 
-validators.maybe = function validateMaybe(x, type, path, context) {
+validators.maybe = function validateMaybe(x, type, path, options) {
   return t.Nil.is(x) ?
     {value: null, errors: []} :
-    recurse(x, type.meta.type, path, context);
+    recurse(x, type.meta.type, path, options);
 };
 
-validators.struct = function validateStruct(x, type, path, context) {
+validators.struct = function validateStruct(x, type, path, options) {
 
   // x should be an object
   if (!t.Obj.is(x)) {
-    return {value: x, errors: [ValidationError.of(x, type, path, context)]};
+    return {value: x, errors: [ValidationError.of(x, type, path, options.context)]};
   }
 
   // [optimization]
@@ -148,7 +148,7 @@ validators.struct = function validateStruct(x, type, path, context) {
   // every item should be of type `props[name]`
   for (var name in props) {
     if (props.hasOwnProperty(name)) {
-      var prop = recurse(x[name], props[name], path.concat(name), context);
+      var prop = recurse(x[name], props[name], path.concat(name), options);
       ret.value[name] = prop.value;
       ret.errors = ret.errors.concat(prop.errors);
     }
@@ -159,31 +159,31 @@ validators.struct = function validateStruct(x, type, path, context) {
   return ret;
 };
 
-validators.tuple = function validateTuple(x, type, path, context) {
+validators.tuple = function validateTuple(x, type, path, options) {
 
   var types = type.meta.types;
   var len = types.length;
 
   // x should be an array of at most `len` items
   if (!t.Arr.is(x) || x.length > len) {
-    return {value: x, errors: [ValidationError.of(x, type, path, context)]};
+    return {value: x, errors: [ValidationError.of(x, type, path, options.context)]};
   }
 
   var ret = {value: [], errors: []};
   // every item should be of type `types[i]`
   for (var i = 0; i < len; i++) {
-    var item = recurse(x[i], types[i], path.concat(i), context);
+    var item = recurse(x[i], types[i], path.concat(i), options);
     ret.value[i] = item.value;
     ret.errors = ret.errors.concat(item.errors);
   }
   return ret;
 };
 
-validators.dict = function validateDict(x, type, path, context) {
+validators.dict = function validateDict(x, type, path, options) {
 
   // x should be an object
   if (!t.Obj.is(x)) {
-    return {value: x, errors: [ValidationError.of(x, type, path, context)]};
+    return {value: x, errors: [ValidationError.of(x, type, path, options.context)]};
   }
 
   var ret = {value: {}, errors: []};
@@ -192,8 +192,8 @@ validators.dict = function validateDict(x, type, path, context) {
   for (var k in x) {
     if (x.hasOwnProperty(k)) {
       var subpath = path.concat(k);
-      var key = recurse(k, type.meta.domain, subpath, context);
-      var item = recurse(x[k], type.meta.codomain, subpath, context);
+      var key = recurse(k, type.meta.domain, subpath, options);
+      var item = recurse(x[k], type.meta.codomain, subpath, options);
       ret.value[k] = item.value;
       ret.errors = ret.errors.concat(key.errors, item.errors);
     }
@@ -201,14 +201,14 @@ validators.dict = function validateDict(x, type, path, context) {
   return ret;
 };
 
-validators.union = function validateUnion(x, type, path, context) {
+validators.union = function validateUnion(x, type, path, options) {
   var ctor = type.dispatch(x);
   return t.Func.is(ctor) ?
-    recurse(x, ctor, path.concat(type.meta.types.indexOf(ctor)), context) :
-    {value: x, errors: [ValidationError.of(x, type, path, context)]};
+    recurse(x, ctor, path.concat(type.meta.types.indexOf(ctor)), options) :
+    {value: x, errors: [ValidationError.of(x, type, path, options.context)]};
 };
 
-validators.intersection = function validateIntersection(x, type, path, context) {
+validators.intersection = function validateIntersection(x, type, path, options) {
 
   var types = type.meta.types;
   var len = types.length;
@@ -216,7 +216,7 @@ validators.intersection = function validateIntersection(x, type, path, context) 
   var ret = {value: x, errors: []};
   // x should be of type `types[i]` for all i
   for (var i = 0; i < len; i++) {
-    var item = recurse(x, types[i], path, context);
+    var item = recurse(x, types[i], path, options);
     ret.errors = ret.errors.concat(item.errors);
   }
   return ret;
